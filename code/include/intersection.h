6ebafd65f33a03971a9f4d1de015420e6fa69448
Max Weltevrede <maxweltevrede@Maxs-MBP.fritz.box>
2018-07-09 19:02:48 +0200

    Add materials and intersections

--- code/include/intersection.h
+++ code/include/intersection.h
@@ -8,6 +8,8 @@
 #ifndef intersection_h
 #define intersection_h
 
+#include "material.h"
+
 /*
  * The type of intersection.
  */
@@ -20,7 +22,7 @@ struct Intersection
 {
 	bool hit = false;
 	Intersect_type type = SPHERE;
-	//Material material;	// material of intersected object
+	Material material;	// material of intersected object
 	Vec3Df normal(0,0,0);
 	Vec3Df hp(0,0,0);		// intersection/hit point
 	
